97905decc8038fa6604d0c3e5f221e51f5b59025
Thomas Druart <thomas.druart.59@gmail.com>
2020-12-16 11:36:59 +0100

    images and portraits added

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -1,15 +1,33 @@
 import React from "react";
-import {} from "./about.style";
+import { Portraits, Member } from "./about.style";
 
 const About = () => {
   return (
     <div>
       <h1>L'Equipe de Toi Demain</h1>
-      <div>
-        <div>Olivia</div>
-        <div>Emilie</div>
-        <div>Christine</div>
-      </div>
+      <Portraits>
+        <Member>
+          <img src="/images/olivia.png" alt="olivia" />
+          <div>
+            <h2>Olivia Duyck</h2>
+            <p>Formatrice / ingénieure pédagogique</p>
+          </div>
+        </Member>
+        <Member>
+          <img src="/images/emilie.png" alt="emilie" />
+          <div>
+            <h2>Emilie Auvray</h2>
+            <p>Formatrice / ingénieure pédagogique</p>
+          </div>
+        </Member>
+        <Member>
+          <img src="/images/christine.png" alt="christine" />
+          <div>
+            <h2>Christine Jubin</h2>
+            <p>Présidente de l’association</p>
+          </div>
+        </Member>
+      </Portraits>
       <p>+ Une équipe de coachs partenaires, formés par Toi Demain!</p>
     </div>
   );
--- src/pages/about/about.style.js
+++ src/pages/about/about.style.js
@@ -1,1 +1,8 @@
 import styled from "styled-components";
+
+export const Portraits = styled.div`
+  display: flex;
+  align-items: row;
+`;
+
+export const Member = styled.div``;
