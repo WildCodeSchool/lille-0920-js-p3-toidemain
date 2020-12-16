4b47400befbb7eaa52bc0d0a52b8d72de092a77d
Guy de Messimy <gdemessimy@gmail.com>
2020-12-16 23:32:01 +0100

    fix problem with CSS import

--- src/components/Dots.jsx
+++ src/components/Dots.jsx
@@ -1,16 +1,18 @@
 import React from 'react'
-import { DotSpan, DotsDiv } from './Dots';
+import { DotSpan, DotsDiv} from './DotsCSS'
 
-const Dot = ({ active }) => (
-  <DotSpan active={active}/>
-)
+function Dot ({ active }) {
+    return (
+        <DotSpan active={active}/>
+    )
+} 
 
 export default function Dots ({ slides, activeIndex }) {
     return (
         <DotsDiv>
             {slides.map((slide, i) => (
             <Dot key={slide} active={activeIndex === i} />
-        ))}
+            ))}
         </DotsDiv>
     )
 }
