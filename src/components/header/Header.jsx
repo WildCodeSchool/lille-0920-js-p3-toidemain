037e247e3147dd8bc0b6761fd211bf41fa2a5dfb
gaetan-cordonnier <gaetan.cordonnier@gmail.com>
2020-12-16 17:05:52 +0100

    Add colors in the header file

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,34 @@
+import { Reset } from "styled-reset";
 import React from "react";
-import { HeaderContainer } from "./header.style";
+import {
+	HeaderContainer,
+	TopBorder,
+	HeaderBackground,
+	LogoContainer,
+	ContactContainer,
+} from "./header.style";
+import Nav from "./Nav";
 
 const Header = () => {
-	return <HeaderContainer></HeaderContainer>;
+	return (
+		<>
+			<Reset />
+			<HeaderContainer>
+				<TopBorder>
+					<p>Association déclarée - SIREN: 852 306 703 00012</p>
+				</TopBorder>
+				<HeaderBackground>
+					<LogoContainer>
+						<img src="/images/logo_Header.png" alt="ToiDemain logo" />
+					</LogoContainer>
+					<ContactContainer>
+						<img src="/images/icons/mail_icon.svg" alt="e-mail icon" />
+						<p>contact@toidemain.fr</p>
+					</ContactContainer>
+				</HeaderBackground>
+				<Nav />
+			</HeaderContainer>
+		</>
+	);
 };
 export default Header;
