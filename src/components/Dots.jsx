f41aa61c81ef4e5d405010d6c508da5c359f0aeb
Guy de Messimy <gdemessimy@gmail.com>
2020-12-16 23:18:35 +0100

    add styled jsx file

--- src/components/Dots.jsx
+++ src/components/Dots.jsx
@@ -1,33 +1,16 @@
 import React from 'react'
-import styled from "styled-components";
-
-const DotSpan = styled.span`
-    padding: 10px;
-    margin-right: 5px;
-    cursor: pointer;
-    border-radius: 50%;
-    background: ${props => props.active ? 'black' : 'white'};
-    `;
+import { DotSpan, DotsDiv } from './Dots';
 
 const Dot = ({ active }) => (
   <DotSpan active={active}/>
 )
 
-const DotsDiv = styled.div`
-    position: absolute;
-    bottom: 25px;
-    width: 100%;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    `;
-
-const Dots = ({ slides, activeIndex }) => (
-  <DotsDiv>
-    {slides.map((slide, i) => (
-      <Dot key={slide} active={activeIndex === i} />
-    ))}
-  </DotsDiv>
-)
-
-export default Dots
+export default function Dots ({ slides, activeIndex }) {
+    return (
+        <DotsDiv>
+            {slides.map((slide, i) => (
+            <Dot key={slide} active={activeIndex === i} />
+        ))}
+        </DotsDiv>
+    )
+}
