03fffb118661aee61ce554db4621bd948a6526dc
AmjadLille <sammanamjad86@gmail.com>
2020-12-16 11:28:51 +0100

    create the main files

---
+++ src/pages/nous-contacter/Contacter.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Contacter = () => {
+  return <div></div>;
+};
+
+export default Contacter;
---
+++ src/pages/nous-contacter/contacter.style.js
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const Adress = styled.div`
+  img {
+    width: 24px;
+  }
+`;
