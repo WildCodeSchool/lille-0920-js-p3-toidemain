034b99e99d30f3426fdfb5b5f2c33ed1e589474c
gaetan-cordonnier <gaetan.cordonnier@gmail.com>
2020-12-16 17:11:20 +0100

    Add module styled-reset on React and Roboto font on index.html file

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import { Reset } from "styled-reset";
 import React from "react";
 import Header from "./components/header/Header";
 
 function App() {
 	return (
 		<div>
+			<Reset />
 			<Header />
 			Hello World
 		</div>
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,4 +1,3 @@
-import { Reset } from "styled-reset";
 import React from "react";
 import {
 	HeaderContainer,
@@ -11,24 +10,21 @@ import Nav from "./Nav";
 
 const Header = () => {
 	return (
-		<>
-			<Reset />
-			<HeaderContainer>
-				<TopBorder>
-					<p>Association déclarée - SIREN: 852 306 703 00012</p>
-				</TopBorder>
-				<HeaderBackground>
-					<LogoContainer>
-						<img src="/images/logo_Header.png" alt="ToiDemain logo" />
-					</LogoContainer>
-					<ContactContainer>
-						<img src="/images/icons/mail_icon.svg" alt="e-mail icon" />
-						<p>contact@toidemain.fr</p>
-					</ContactContainer>
-				</HeaderBackground>
-				<Nav />
-			</HeaderContainer>
-		</>
+		<HeaderContainer>
+			<TopBorder>
+				<p>Association déclarée - SIREN: 852 306 703 00012</p>
+			</TopBorder>
+			<HeaderBackground>
+				<LogoContainer>
+					<img src="/images/logo_Header.png" alt="ToiDemain logo" />
+				</LogoContainer>
+				<ContactContainer>
+					<img src="/images/icons/mail_icon.svg" alt="e-mail icon" />
+					<p>contact@toidemain.fr</p>
+				</ContactContainer>
+			</HeaderBackground>
+			<Nav />
+		</HeaderContainer>
 	);
 };
 export default Header;
