5bf358b009757c4d720a4d4a5779fbc4e38d883d
AmjadLille <sammanamjad86@gmail.com>
2020-12-18 01:41:42 +0100

    Prettify

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,5 +1,13 @@
 import React from "react";
-import { FooterContainer, Info, Adress, Middle, Menu, Social, Icons } from "./style";
+import {
+  FooterContainer,
+  Info,
+  Adress,
+  Middle,
+  Menu,
+  Social,
+  Icons,
+} from "./style";
 
 const Footer = () => {
   return (
