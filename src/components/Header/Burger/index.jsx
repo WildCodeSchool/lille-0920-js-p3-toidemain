f516099ca20fdf8e9182d8f8e6ee72e2bebb6d22
Gaëtan Cordonnier <69042173+gaetan-cordonnier@users.noreply.github.com>
2021-01-04 10:31:16 +0100

    Update index.jsx
    Delete empty lines

--- src/components/Header/Burger/index.jsx
+++ src/components/Header/Burger/index.jsx
@@ -6,14 +6,10 @@ export default function Burger() {
   return (
     <StyledBurgerMenu>
       <Menu right width={"180px"}>
-        <a href="/">Nous</a>
-
+        <a href="/">Nous</a
         <a href="/actions">Actions</a>
-
         <a href="/soutien">Soutien</a>
-
         <a href="/actualite">Actualité</a>
-
         <a href="/contact">Contact</a>
       </Menu>
     </StyledBurgerMenu>
