add57025b5e053548ed3a9ae8b165401bb7b1fbc
Amelie Macquet <amelie.macquet.am@gmail.com>
2021-01-06 00:37:04 +0100

    add structure of pedagogie file

---
+++ src/components/Pedagogie/index.jsx
@@ -0,0 +1,63 @@
+import React from "react";
+
+const Pedagogie = () => {
+  return (
+    <div>
+      <div>
+        <h2>NOTRE h2</h2>
+        <div>
+          <p>
+            <span>Toi Demain</span> a une pédagogie d’apprentissage basée sur
+            l’expérimentation. Il est essentiel pour nos équipes de créer
+            l’alliance avec les jeunes et de leur permettre d’apprendre en
+            s’amusant.
+          </p>
+        </div>
+
+        <div>
+          <div>
+            <h2>TOUT CELA DANS L'h2 DE</h2>
+            <ul>
+              <li>
+                Susciter des envies et affiner le projet professionnel des
+                jeunes
+              </li>
+              <li>
+                Leur permettre d’être acteur de leur stage en mettant en place
+                un projet
+              </li>
+              <li>
+                Travailler la posture à l’oral pour les présentations et les
+                entretiens
+              </li>
+              <li>S’épanouir au sein d’un collectif</li>
+              <li>
+                S’adapter aux codes de l’entreprise et découvrir toutes les
+                opportunités qu’elle offre
+              </li>
+              <li>
+                Développer la connaissance de soi et faire grandir la confiance
+                en soi
+              </li>
+            </ul>
+          </div>
+          <p>
+            POUR leur permettre d’être
+            <span> libres, responsables et heureux.</span>
+          </p>
+        </div>
+
+        <div>
+          <p>
+            Nous sommes en partenariat avec <span>ORIENTOI</span> qui propose
+            des jeux au sujet de l’orientation. Les jeunes réalisent le test en
+            ligne pendant le stage.
+          </p>
+          <img src="/images/orientoi.png" alt="logo oriente Toi" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Pedagogie;
