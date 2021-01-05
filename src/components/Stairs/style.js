e7ba4587e7a38b2be71001d051763ab43b2297a7
Amelie Macquet <amelie.macquet.am@gmail.com>
2021-01-05 23:42:51 +0100

    add responsive and apply prettier

--- src/components/Stairs/style.js
+++ src/components/Stairs/style.js
@@ -1,9 +1,11 @@
-import styled from "styled-components";
+import styled from 'styled-components'
+import { device } from '../../layout/Devices'
 
 export const Box1 = styled.div`
   background-color: #cfd7d6;
-  font-family: "Oswald", Gadget, sans-serif;
-  font-size: 20px;
+  font-family: 'Oswald', Gadget, sans-serif;
+  font-size: 15px;
+  padding-top: 5px;
   color: white;
   width: 120px;
   height: 80px;
@@ -19,27 +21,29 @@ export const Box1 = styled.div`
   }
 `
 export const Box2 = styled.div`
-background-color: #1c819e;
-font-family: "Oswald", Gadget, sans-serif;
-font-size: 20px;
-color: white;
-width: 120px;
-height: 120px;
-transition: height 0.5s;
-transition-timing-function: linear;
-display: flex;
-justify-content: center;
-text-align: center;
-margin: 3px;
-border-radius: 9px;
-&:hover {
-  height: 170px;
-}
-`;
+  background-color: #1c819e;
+  font-family: 'Oswald', Gadget, sans-serif;
+  font-size: 15px;
+  padding-top: 5px;
+  color: white;
+  width: 120px;
+  height: 120px;
+  transition: height 0.5s;
+  transition-timing-function: linear;
+  display: flex;
+  justify-content: center;
+  text-align: center;
+  margin: 3px;
+  border-radius: 9px;
+  &:hover {
+    height: 170px;
+  }
+`
 export const Box3 = styled.div`
   background-color: #005542;
-  font-family: "Oswald", Gadget, sans-serif;
-  font-size: 20px;
+  font-family: 'Oswald', Gadget, sans-serif;
+  font-size: 15px;
+  padding-top: 5px;
   color: white;
   width: 120px;
   height: 170px;
@@ -53,11 +57,12 @@ export const Box3 = styled.div`
   &:hover {
     height: 220px;
   }
-`;
+`
 export const Box4 = styled.div`
   background-color: #145264;
-  font-family: "Oswald", Gadget, sans-serif;
-  font-size: 20px;
+  font-family: 'Oswald', Gadget, sans-serif;
+  font-size: 15px;
+  padding-top: 5px;
   color: white;
   width: 120px;
   height: 220px;
@@ -71,11 +76,12 @@ export const Box4 = styled.div`
   &:hover {
     height: 270px;
   }
-`;
+`
 export const Box5 = styled.div`
   background-color: #f5e01d;
-  font-family: "Oswald", Gadget, sans-serif;
-  font-size: 20px;
+  font-family: 'Oswald', Gadget, sans-serif;
+  font-size: 15px;
+  padding-top: 5px;
   color: white;
   width: 120px;
   height: 270px;
@@ -89,4 +95,28 @@ export const Box5 = styled.div`
   &:hover {
     height: 330px;
   }
-`;
+`
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100vw;
+  background-color: green;
+  @media ${device.mobile} {
+    display: none;
+  }
+  @media ${device.tablet} {
+    display: none;
+  }
+`
+export const Stairs = styled.div`
+  width: 90%;
+  display: flex;
+  align-items: flex-end;
+  justify-content: center;
+`
+export const Title = styled.h2`
+  font-family: 'Oswald', Gadget, sans-serif;
+  font-size: 25px;
+  margin-top: 20px;
+`
