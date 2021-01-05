f650bc0c17f54f92509e26599202941b9ee5b5a7
Amelie Macquet <amelie.macquet.am@gmail.com>
2021-01-05 22:40:31 +0100

    add title to the box

--- src/components/Stairs/stairs.jsx
+++ src/components/Stairs/stairs.jsx
@@ -4,11 +4,11 @@ const Stairs = () => {
   return (
     <div>
       <div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
+        <div><p>S'INSPIRER</p></div>
+        <div><p>SE CONNAITRE</p></div>
+        <div><p>S'ORIENTER</p></div>
+        <div><p>EXPERIMENTER</p></div>
+        <div><p>SE CHALLENGER</p></div>
       </div>
       <h2>S'épanouir en s'amusant</h2>
     </div>
