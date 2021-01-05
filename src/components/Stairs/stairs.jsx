abc8c7c895b6ca52f5357a540eb1acbb54ffd1fe
Amelie Macquet <amelie.macquet.am@gmail.com>
2021-01-05 22:36:41 +0100

    modification file's name and add structure of the animation

---
+++ src/components/Stairs/stairs.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Stairs = () => {
+  return (
+    <div>
+      <div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+      <h2>S'épanouir en s'amusant</h2>
+    </div>
+  );
+};
+export default Stairs;
