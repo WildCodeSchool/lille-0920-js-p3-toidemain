eeb0138fa5f167519000cb46afaa8c1d8412ffe6
Amelie Macquet <amelie.macquet.am@gmail.com>
2021-01-06 11:31:45 +0100

    add modifications to container2

--- src/components/Pedagogie/style.js
+++ src/components/Pedagogie/style.js
@@ -1,23 +1,27 @@
-import styled from 'styled-components'
+import styled from "styled-components";
 
-export const Title = styled.h2 ` 
-font-family: "Oswald", Gadget, sans-serif;
-  font-size: 50px;
+export const PedaContainer = styled.div`
+  font-family: "Oswald", Gadget, sans-serif;
+  font-size: 25px;
+`;
+
+export const Title = styled.h2`
+  font-size: 2.5rem;
   padding-bottom: 20px;
   color: #f5e01d;
   margin: 25px;
-`
-export const Intro = styled.p ` 
- font-family: "Arial Black", Gadget, sans-serif;
-  font-size: 25px;
+`;
+export const Intro = styled.p`
+  font-family: "Arial Black", Gadget, sans-serif;
   width: 20vw;
   text-align: center;
   color: white;
-  background-color: grey;
+  background-color: #005542;
+  opacity: 80%;
   border-radius: 5px;
   padding: 40px;
   margin-left: 70px;
-`
+`;
 export const CapitalL = styled.span`
   font-size: 45px;
 `;
@@ -27,55 +31,66 @@ export const Container1 = styled.div`
   margin-left: 140px;
 `;
 export const Container2 = styled.div`
-  background-color:  #cfd7d6;
-  width:100vw;
+  background-color: #145264;
+  background: rgba(0, 0, 0, 0.5);
+  width: 100vw;
   margin-top: 8vh;
+  display: flex;
 `;
 export const Objectif = styled.div`
-  font-family: "Oswald", Gadget, sans-serif;
   text-align: center;
-  
+  padding-top: 4vh;
+  display: flex;
 `;
-export const Title2 = styled.h2 ` 
- font-family: "Oswald", Gadget, sans-serif;
-  font-size: 40px;
-  color: #1c819e;
-  margin-top: 5vh;
+export const Title2 = styled.h2`
+  font-size: 5rem;
+  color: #f5e01d;
+  width: 30vw;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 9px;
 `;
 export const Liste = styled.ul`
-  margin-top: 60px;
+  width: 35vw;
+  text-align: justify;
+  display: flex;
+  flex-direction: column;
+  margin-top: -4vh;
+  margin-left: 3vw;
 `;
 
 export const ListeElement = styled.li`
-  font-size: 25px;
-  color: black;
+  color: white;
   padding: 10px;
 `;
 export const Slogan = styled.p`
+  width: 30vw;
   text-align: center;
-  font-family: "Oswald", Gadget, sans-serif;
-  font-size: 40px;
-  padding: 50px;
+  color: white;
+  font-size: 3rem;
+  margin-top: 9vh;
 `;
-export const Color = styled.span ` 
-font-size: 50px;
-  color: #f5e01d;
-`
+export const Color = styled.span`
+  font-size: 50px;
+  color: #145264;
+`;
 export const Container3 = styled.div`
-display: flex;
+  display: flex;
   flex-direction: column;
   align-items: center;
   margin-top: 5vh;
-`
+`;
 export const Logo = styled.img`
-font-size: 60px;
+  font-size: 60px;
   color: #005542;
-`
+`;
 export const Orientoi = styled.p`
- text-align: center;
-  font-family: "Oswald", Gadget, sans-serif;
-  font-size: 30px;
-  margin-left:14vw;
+  text-align: center;
+  margin-left: 14vw;
   margin-right: 14vw;
 `;
-`
+export const ColorB = styled.span`
+  color: #f5e01d;
+  font-size: 50px;
+`;
