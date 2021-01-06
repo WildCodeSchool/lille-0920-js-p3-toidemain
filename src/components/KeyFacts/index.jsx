680be1be670ae4fceb3a5e99c4ce595938f413b0
Thomas Druart <thomas.druart.59@gmail.com>
2021-01-06 10:57:47 +0100

    icons import

--- src/components/KeyFacts/index.jsx
+++ src/components/KeyFacts/index.jsx
@@ -6,14 +6,14 @@ const KeyFacts = () => {
   return (
     <KeyFactsContainer>
       <Fact>
-        <p>icone</p>
+        <img src="/images/icons/bulles.png" alt="bulles" />
         <p>100%</p>
         <p>
           des jeunes recommandent à leurs copains de participer à notre stage.
         </p>
       </Fact>
       <Fact>
-        <p>icone</p>
+        <img src="/images/icons/loupe.png" alt="loupe" />
         <p>100%</p>
         <p>
           de nos partenaires estiment que notre formule de stage est bénéfique
@@ -21,7 +21,7 @@ const KeyFacts = () => {
         </p>
       </Fact>
       <Fact>
-        <p>icone</p>
+        <img src="/images/icons/plus.png" alt="plus" />
         <p>100%</p>
         <p>
           des jeunes sont capables de présenter leur projet pro ou leur atouts à
@@ -29,7 +29,7 @@ const KeyFacts = () => {
         </p>
       </Fact>
       <Fact>
-        <p>icone</p>
+        <img src="/images/icons/coche.png" alt="coche" />
         <p>100%</p>
         <p>de nos partenaires recommandent l’action à d’autres entreprises.</p>
       </Fact>
--- src/components/KeyFacts/style.js
+++ src/components/KeyFacts/style.js
@@ -10,4 +10,8 @@ export const Fact = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
+
+  img {
+    width: 30px;
+  }
 `;
