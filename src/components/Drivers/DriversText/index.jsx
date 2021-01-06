82d159c364899026207ec590da211e997d31a76b
gaetan-cordonnier <gaetan.cordonnier@gmail.com>
2021-01-06 16:38:26 +0100

    Added images for driver component

---
+++ src/components/Drivers/DriversText/index.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { TextContainer } from "./style";
+
+export default function DriversText() {
+	return (
+		<TextContainer>
+			<p>Example</p>
+		</TextContainer>
+	);
+}
---
+++ src/components/Drivers/DriversText/style.js
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const TextContainer = styled.div`
+	width: 200px;
+	height: 100px;
+	border: 2px solid #005542;
+`;
