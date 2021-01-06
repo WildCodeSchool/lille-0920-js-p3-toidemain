3244a1e98bce76d63d4a997e69bfd0804d93a2dc
Amelie Macquet <amelie.macquet.am@gmail.com>
2021-01-06 10:14:53 +0100

    Add style component to the file

---
+++ src/components/Pedagogie/style.js
@@ -0,0 +1,20 @@
+import styled from 'styled-components'
+
+export const Title = styled.h2 ` 
+font-family: "Oswald", Gadget, sans-serif;
+  font-size: 50px;
+  padding-bottom: 20px;
+  color: #f5e01d;
+  margin: 25px;
+`
+export const Intro = styled.p ` 
+ font-family: "Arial Black", Gadget, sans-serif;
+  font-size: 25px;
+  width: 20vw;
+  text-align: center;
+  color: white;
+  background-color: grey;
+  border-radius: 5px;
+  padding: 40px;
+  margin-left: 70px;
+`
