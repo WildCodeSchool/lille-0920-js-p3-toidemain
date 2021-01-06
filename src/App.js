3f4bf82de614eaf74ab88f9788e37b8502408dfc
Amelie Macquet <amelie.macquet.am@gmail.com>
2021-01-06 14:57:48 +0100

    add components to see

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React from "react";
 import Header from "./components/Header/index";
 import About from "./pages/about/About";
 import Footer from "./components/Footer/index";
+import Pedagogie from "./components/Pedagogie/index";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Header />
       <About />
       <Footer />
+      <Pedagogie />
     </div>
   );
 }
