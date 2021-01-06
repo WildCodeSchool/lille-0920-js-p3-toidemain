7d708076a09132a4a2d065aea5397c5afbcaed4b
Amelie Macquet <amelie.macquet.am@gmail.com>
2021-01-06 10:34:25 +0100

    Add style to container12 & 3

--- src/components/Pedagogie/style.js
+++ src/components/Pedagogie/style.js
@@ -26,3 +26,56 @@ export const Container1 = styled.div`
   justify-content: center;
   margin-left: 140px;
 `;
+export const Container2 = styled.div`
+  background-color:  #cfd7d6;
+  width:100vw;
+  margin-top: 8vh;
+`;
+export const Objectif = styled.div`
+  font-family: "Oswald", Gadget, sans-serif;
+  text-align: center;
+  
+`;
+export const Title2 = styled.h2 ` 
+ font-family: "Oswald", Gadget, sans-serif;
+  font-size: 40px;
+  color: #1c819e;
+  margin-top: 5vh;
+`;
+export const Liste = styled.ul`
+  margin-top: 60px;
+`;
+
+export const ListeElement = styled.li`
+  font-size: 25px;
+  color: black;
+  padding: 10px;
+`;
+export const Slogan = styled.p`
+  text-align: center;
+  font-family: "Oswald", Gadget, sans-serif;
+  font-size: 40px;
+  padding: 50px;
+`;
+export const Color = styled.span ` 
+font-size: 50px;
+  color: #f5e01d;
+`
+export const Container3 = styled.div`
+display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 5vh;
+`
+export const Logo = styled.img`
+font-size: 60px;
+  color: #005542;
+`
+export const Orientoi = styled.p`
+ text-align: center;
+  font-family: "Oswald", Gadget, sans-serif;
+  font-size: 30px;
+  margin-left:14vw;
+  margin-right: 14vw;
+`;
+`
