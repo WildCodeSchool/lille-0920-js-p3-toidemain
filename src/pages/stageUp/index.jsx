41bc154206d162384083495e00c7a0c8465abbba
AmjadLille <sammanamjad86@gmail.com>
2021-01-07 12:07:33 +0100

    fin first page

--- src/pages/stageUp/index.jsx
+++ src/pages/stageUp/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Main, First, Second, Third, Forth, FirstTextContainer, Title, List, Text, FirstUp, Firstdown } from './style';
+import { Main, First, Second, Third, Forth, FirstTextContainer, Title, ListR, ListL, TextR, TextL, FirstUp, Firstdown, SecText, ThirdContainer } from './style';
 
 const StageUp = () => {
   return (
@@ -8,7 +8,12 @@ const StageUp = () => {
         <FirstTextContainer>
           <img src="/images/stageup.png" alt="stageup" />
           <Firstdown>
-            <h3>Les avantages de notre programme : Permettre aux jeunes de vivre un premier contact positif avec l’entreprise. Offrir à un plus grand nombre de jeunes un stage de qualité Permettre aux jeunes des REP, ou à ceux qui n’ont pas de réseau d’accéder à un stage en entreprise S’impliquer d’avantage dans l’orientation des jeunes en tant qu’entreprise Maitriser les temps d’échanges passés entre les jeunes et les collaborateurs</h3>
+            <h3>
+              Les avantages de notre programme :<br /> Permettre aux jeunes de vivre un premier contact positif avec l’entreprise.
+              <br /> Offrir à un plus grand nombre de jeunes un stage de qualité <br />
+              Permettre aux jeunes des REP, ou à ceux qui n’ont pas de réseau d’accéder à un stage en entreprise <br />
+              S’impliquer d’avantage dans l’orientation des jeunes en tant qu’entreprise Maitriser les temps d’échanges passés entre les jeunes et les collaborateurs
+            </h3>
           </Firstdown>
         </FirstTextContainer>
         <FirstUp>
@@ -20,34 +25,47 @@ const StageUp = () => {
         </FirstUp>
       </First>
       <Second>
-        <Text> Stage up c’est : 1 semaine de stage collectif innovante au sein d’une entreprise Pour un groupe de 12 à 20 collégiens / lycéens Encadré H24 par 2 coachs certifiés à l`orientation scolaire Pour l’entreprise une action RSE, clé en main, et aussi un avantage pour les enfants de collaborateurs L’action est finançable à 100% via la taxe d’apprentissage</Text>
+        <img src="/images/hands.jpg" alt="hands" />
+        <SecText>
+          <h2>
+            <strong>Stage up c’est :</strong>
+            <br /> - <span>1</span> semaine de stage collectif innovante au sein d’une entreprise
+            <br /> - Pour un groupe de <sp>12 à 20</sp> collégiens / lycéens
+            <br /> - Encadré <span>H24</span> par <span>2</span> coachs certifiés à l`orientation scolaire
+            <br /> - Pour l’entreprise une action <span>RSE, clé en main</span>, et aussi un avantage pour les enfants de collaborateurs
+            <br /> - L’action est finançable à <span>100%</span> via la taxe d’apprentissage
+          </h2>
+        </SecText>
       </Second>
 
       <Third>
         <Title>
           <h1>Concrètement ça se passe comment?</h1>
         </Title>
-        <Title>
-          <h2>Toi Demain Vous</h2>
-        </Title>
-        <List>
-          <li>Mise à dispo d`une salle.</li>
-          <li>4 collaborateurs 3x2heures.</li>
-          <li>2h pour le porteur de projet.</li>
-          <li>1 collaborateur 1x1h : les métiers d’avenir</li>
-        </List>
-        <List>
-          <li>Recrutement des stagiaires et gestion des conventions de stages.</li>
-          <li>Encadrement et animation des jeunes pendant toute la durée du stage.</li>
-          <li> Accompagnement des collaborateurs intervenants.</li>
-        </List>
+
+        <ThirdContainer>
+          <ListR>
+            <li>Mise à dispo d`une salle.</li>
+            <li>4 collaborateurs 3x2heures.</li>
+            <li>2h pour le porteur de projet.</li>
+            <li>1 collaborateur 1x1h : les métiers d’avenir</li>
+          </ListR>
+          <img src="/images/vous.png" alt="toi domain et vous" />
+          <ListL>
+            <li>Recrutement des stagiaires et gestion des conventions de stages.</li>
+            <li>Encadrement et animation des jeunes pendant toute la durée du stage.</li>
+            <li> Accompagnement des collaborateurs intervenants.</li>
+          </ListL>
+        </ThirdContainer>
       </Third>
       <Forth>
-        <Title>
-          <h2>Quand? Où?</h2>
-        </Title>
-        <Text>Une salle de réunion au sein de l’entreprise prévue pour 12 à 20 jeunes + 2 animateurs toute la semaine.</Text>
-        <Text>Pendant les périodes de stages prévu par l’éducation nationale. Principalement novembre / décembre / janvier / février / juin</Text>
+        <TextR>
+          <p>Une salle de réunion au sein de l’entreprise prévue pour 12 à 20 jeunes + 2 animateurs toute la semaine.</p>
+        </TextR>
+        <img src="/images/quand.png" alt="quand et ou" />
+        <TextL>
+          <p>Pendant les périodes de stages prévu par l’éducation nationale. Principalement novembre / décembre / janvier / février / juin</p>
+        </TextL>
       </Forth>
     </Main>
   );
--- src/pages/stageUp/style.js
+++ src/pages/stageUp/style.js
@@ -5,11 +5,14 @@ export const Main = styled.div`
   height: 90vh;
   overflow-y: scroll;
   scroll-snap-type: y mandatory;
-  background-color: gray;
-  -webkit-background-size: cover;
+  /* background-image: url('/images/background-white.jpg'); */
+  background-color: white;
+  /* -webkit-background-size: cover;
   -moz-background-size: cover;
   -o-background-size: cover;
-  background-size: cover;
+  background-size: cover; */
+  font-family: 'Roboto', sans-serif;
+  color: #02523f;
 `;
 
 export const First = styled.section`
@@ -20,72 +23,162 @@ export const First = styled.section`
   justify-content: space-around;
 `;
 export const FirstTextContainer = styled.div`
-  margin-top: 5vh;
   width: 50%;
   display: flex;
   flex-direction: column;
-  justify-content: left;
+  justify-content: center;
+
   img {
-    width: 27vw;
+    width: 22vw;
     height: 40vh;
-    margin-left: 5vw;
+    margin-left: 15vw;
+    margin-top: 5vh;
   }
 `;
 export const FirstUp = styled.div`
   width: 50%;
+  margin-top: 10vh;
   h3 {
     text-align: center;
     font-size: 2em;
     width: 80%;
     margin: 5vh auto;
     line-height: 1.5em;
+    padding: 5px;
   }
   span {
     font-size: 1.5em;
-    color: #ffbe00;
+    color: #d6a002;
   }
 `;
 export const Firstdown = styled.div`
-  margin-bottom: 10vh;
+  background: rgba(28, 129, 158, 0.3);
+  border-radius: 20px;
+  width: 80%;
+  margin-left: 5vw;
+  margin-bottom: 20vh;
+  height: 90%;
+
   h3 {
-    font-size: 1.75em;
-    font-weight: bold;
-    width: 100%;
-    margin-left: 5vw;
+    font-size: 1.5em;
+    width: 90%;
+    margin: auto;
     line-height: 1.5em;
+    padding: 5px;
+    text-align: left;
   }
 `;
 
 export const Second = styled.section`
   height: 90vh;
   scroll-snap-align: center;
+  display: flex;
+  flex-direction: row-reverse;
+  justify-content: center;
+  img {
+    width: 55%;
+  }
+`;
+export const SecText = styled.div`
+  margin: auto 5vw auto 2vw;
+  width: 45%;
+  span {
+    font-size: 1.5em;
+    color: #d6a002;
+  }
+  h2 {
+    font-size: 1.75em;
+    width: 80%;
+    line-height: 2em;
+  }
 `;
 
 export const Third = styled.section`
   height: 90vh;
   scroll-snap-align: center;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
 `;
 export const Title = styled.div`
+  margin: 8vh auto;
   h1 {
-    font-size: 2.5em;
-  }
-  h2 {
-    font-size: 2em;
-  }
-  h3 {
-    font-size: 1.75em;
+    font-size: 3.5em;
+    text-align: center;
   }
 `;
-export const List = styled.ul`
+export const ThirdContainer = styled.div`
+  display: flex;
+  flex-direction: row-reverse;
+  justify-content: space-around;
+  align-items: center;
+  padding-bottom: 20vh;
+  img {
+    width: 40%;
+  }
+`;
+
+export const ListR = styled.ul`
+  background: rgba(28, 129, 158, 0.3);
+  width: 30%;
+  margin: auto 0;
+  height: 100%;
+
   li {
     font-size: 2em;
+    width: 90%;
+    margin: 0 2vw;
+    color: #04345e;
   }
 `;
-export const Text = styled.p`
-  font-size: 1.5em;
+export const ListL = styled.ul`
+  background: rgba(28, 129, 158, 0.3);
+  width: 30%;
+  height: 100%;
+
+  li {
+    font-size: 1.8em;
+    width: 90%;
+    margin: 3vh auto;
+    line-height: 1.25em;
+    color: #0f4d19;
+  }
 `;
 
 export const Forth = styled.section`
   height: 90vh;
   scroll-snap-align: center;
+  display: flex;
+  flex-direction: row-reverse;
+  justify-content: space-around;
+  align-items: center;
+  img {
+    width: 40%;
+  }
+`;
+export const TextR = styled.div`
+  background: rgba(28, 129, 158, 0.3);
+  width: 30%;
+  height: 40%;
+  p {
+    font-size: 2em;
+    line-height: 1.75em;
+    width: 90%;
+    margin: 10vh 2vw;
+    color: #d6a002;
+    text-align: center;
+  }
+`;
+export const TextL = styled.div`
+  background: rgba(28, 129, 158, 0.3);
+  height: 40%;
+  width: 30%;
+  p {
+    font-size: 2em;
+    line-height: 1.75em;
+    width: 90%;
+    margin: 10vh 1vw;
+    color: #04345e;
+    text-align: center;
+  }
 `;
