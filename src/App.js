28d220456c98d59fe7173ec0dd5a5135d5f35409
Thomas Druart <68994903+ThomasDruart@users.noreply.github.com>
2021-01-11 15:32:36 +0100

    Update App.js
    empty line deleted

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { Reset } from "styled-reset";
 import Contacter from "./pages/nous-contacter/index";
-
 import About from "./pages/About";
 import Header from "./components/Header/index";
 import Footer from "./components/Footer/index";
