72f2060d8f44e7890733790214c1ac3bef4b4f95
AmjadLille <sammanamjad86@gmail.com>
2021-01-11 13:45:51 +0100

    fixing

--- src/components/Drivers/style.js
+++ src/components/Drivers/style.js
@@ -1,65 +1,32 @@
 import styled from 'styled-components';
-export const DriversStyle = styled.div`
-  svg {
-  }
 
+export const DriversStyle = styled.div`
+  background-image: url(/images/animated_background.svg);
+  font-family: 'Roboto', sans-serif;
   svg g {
-    fill: #0698b6;
+    fill: #1c819e;
+  }
+  svg g:hover {
+    fill: #91ccdb;
+    cursor: pointer;
+  }
+  svg g polygon {
+    stroke: #d7d7d7;
+    stroke-width: 0.5;
+    transition: fill-opacity 0.5s;
+  }
+  svg g text {
+    font-size: 0.13em;
+    text-transform: uppercase;
+    fill: #fff;
+  }
+  .hexBlue g {
+    fill: #1c819e;
+  }
+  .hexGreen g {
+    fill: #005542;
+  }
+  .hexYellow g {
+    fill: #ffbe00;
   }
 `;
-// svg {
-// }
-
-// Russian blue pattern:
-// #7be3f6
-// #4499a9
-// #276a76
-// #133b43
-// #03090a
-
-// svg g {
-// 	fill: #0698b6;
-// }
-
-// svg g:hover {
-// 	fill: #91ccdb;
-// }
-// svg g:hover text {
-// 	fill-opacity: 1;
-// }
-
-// svg g polygon {
-// 	stroke: transparent;
-// 	stroke-width: 0.2;
-// 	transition: fill-opacity 0.5s;
-// }
-// svg g text {
-// 	font-size: 0.12em;
-// 	fill: #fff;
-// 	transition: fill-opacity 0.5s;
-// }
-// svg path {
-// 	fill: none;
-// 	stroke: #7be3f6;
-// 	stroke-width: 0.2em;
-// 	stroke-opacity: 0.7;
-// 	stroke-linecap: round;
-// 	stroke-linejoin: round;
-// }
-
-// /*
-// #005542
-// #1C819E
-// #FFBE00
-// */
-// .hexBlue g {
-// 	fill: #1c819e;
-// }
-
-// .hexGreen g {
-// 	fill: #005542;
-// }
-
-// .hexYellow g {
-// 	fill: #ffbe00;
-// }
