1230b8d1528801e25c46de6160786cc2a0a44b17
de Messimy <69811970+Guy-deMessimy@users.noreply.github.com>
2021-01-11 11:29:31 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,14 +3,12 @@ import React from "react";
 import Header from "./components/Header/index";
 import About from "./pages/about/About";
 import Footer from "./components/Footer/index";
-import Drivers from "./components/Drivers/index";
 
 function App() {
 	return (
 		<div>
 			<Reset />
 			<Header />
-			<Drivers />
 			<About />
 			<Footer />
 		</div>
