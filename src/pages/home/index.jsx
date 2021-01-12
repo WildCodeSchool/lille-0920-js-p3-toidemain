395a8a2eea4819679a6b1f6fe6a2a1f03f88b7ec
Guy de Messimy <gdemessimy@gmail.com>
2021-01-12 15:19:23 +0100

    Prettify

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -3,18 +3,17 @@ import MenuButton from "../../components/HomeButton";
 import Slider from "../../components/MySlider/Slider";
 import images from "../../components/MySlider/img/images";
 import { HomeContainer } from "./style";
-import Quote from "../../components/MyQuote/index"
+import Quote from "../../components/MyQuote/index";
 
 const Home = () => {
   return (
-      <div>
-        <HomeContainer>
-      <Slider slides={images} autoPlay={50} />
-      <MenuButton />
-    </HomeContainer>
-    <Quote />
-      </div>
-    
+    <div>
+      <HomeContainer>
+        <Slider slides={images} autoPlay={50} />
+        <MenuButton />
+      </HomeContainer>
+      <Quote />
+    </div>
   );
 };
 
