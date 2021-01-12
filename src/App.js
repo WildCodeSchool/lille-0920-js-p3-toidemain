331e47abbb5e42783a33ff4d1413e910db69f8d0
Thomas Druart <thomas.druart.59@gmail.com>
2021-01-12 16:40:20 +0100

    router done on logo

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { Reset } from "styled-reset";
 import { AppStyle } from "./style";
+import Home from "./pages/home";
 import Contacter from "./pages/nous-contacter/index";
 import About from "./pages/about";
 import Footer from "./components/Footer/index";
@@ -11,23 +12,23 @@ import Pedagogie from "./components/Pedagogie/index";
 import Atelier from "./components/Atelier";
 
 function App() {
-	return (
-		<AppStyle>
-			<Reset />
-			<Router>
-				<Header />
-				<Switch>
-					<Route exact path="/" component={About} />
-					<Route exact path="/about" component={About} />
-					<Route exact path="/contacter" component={Contacter} />
-					<Route exact path="/stageup" component={StageUp} />
-					<Route exact path="/action/pedagogie" component={Pedagogie} />
-					<Route exact path="/action/atelier" component={Atelier} />
-				</Switch>
-				<Footer />
-			</Router>
-		</AppStyle>
-	);
+  return (
+    <AppStyle>
+      <Reset />
+      <Router>
+        <Header />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/about" component={About} />
+          <Route exact path="/contacter" component={Contacter} />
+          <Route exact path="/stageup" component={StageUp} />
+          <Route exact path="/action/pedagogie" component={Pedagogie} />
+          <Route exact path="/action/atelier" component={Atelier} />
+        </Switch>
+        <Footer />
+      </Router>
+    </AppStyle>
+  );
 }
 
 export default App;
