1c7dfae365253a474bd884426c20699545a3cfff
gaetan-cordonnier <gaetan.cordonnier@gmail.com>
2021-01-12 15:30:53 +0100

    Background added

--- src/App.js
+++ src/App.js
@@ -1,32 +1,33 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import { Reset } from 'styled-reset';
-import Contacter from './pages/nous-contacter/index';
-import About from './pages/about/index';
-import Footer from './components/Footer/index';
-import Header from './components/Header/index';
-import StageUp from './pages/stageUp/index';
-import Pedagogie from './components/Pedagogie/index';
-import Atelier from './components/Atelier';
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { Reset } from "styled-reset";
+import { AppStyle } from "./style";
+import Contacter from "./pages/nous-contacter/index";
+import About from "./pages/about/index";
+import Footer from "./components/Footer/index";
+import Header from "./components/Header/index";
+import StageUp from "./pages/stageUp/index";
+import Pedagogie from "./components/Pedagogie/index";
+import Atelier from "./components/Atelier";
 
 function App() {
-  return (
-    <>
-      <Reset />
-      <Router>
-        <Header />
-        <Switch>
-          <Route exact path="/" component={About} />
-          <Route exact path="/about" component={About} />
-          <Route exact path="/contacter" component={Contacter} />
-          <Route exact path="/stageup" component={StageUp} />
-          <Route exact path="/action/pedagogie" component={Pedagogie} />
-          <Route exact path="/action/atelier" component={Atelier} />
-        </Switch>
-        <Footer />
-      </Router>
-    </>
-  );
+	return (
+		<AppStyle>
+			<Reset />
+			<Router>
+				<Header />
+				<Switch>
+					<Route exact path="/" component={About} />
+					<Route exact path="/about" component={About} />
+					<Route exact path="/contacter" component={Contacter} />
+					<Route exact path="/stageup" component={StageUp} />
+					<Route exact path="/action/pedagogie" component={Pedagogie} />
+					<Route exact path="/action/atelier" component={Atelier} />
+				</Switch>
+				<Footer />
+			</Router>
+		</AppStyle>
+	);
 }
 
 export default App;
--- src/components/Header/style.js
+++ src/components/Header/style.js
@@ -2,184 +2,184 @@ import styled from "styled-components";
 import { device } from "../../layout/Devices";
 
 export const HeaderContainer = styled.div`
-  position: relative;
-  display: flex;
-  flex-direction: column;
-  align-content: flex-start;
-  justify-content: center;
-  height: 200px;
-  width: 100%;
-  background: #e6e6e6;
+	position: relative;
+	display: flex;
+	flex-direction: column;
+	align-content: flex-start;
+	justify-content: center;
+	height: 200px;
+	width: 100%;
+	background: transparent;
 
-  @media ${device.small_screen} {
-    height: 140px;
-  }
+	@media ${device.small_screen} {
+		height: 140px;
+	}
 
-  @media ${device.tablet} {
-    height: 140px;
-  }
+	@media ${device.tablet} {
+		height: 140px;
+	}
 
-  @media ${device.mobile} {
-    height: 80px;
-  }
+	@media ${device.mobile} {
+		height: 80px;
+	}
 `;
 
 export const TopBorder = styled.div`
-  position: absolute;
-  top: 0;
-  display: flex;
-  flex-direction: row;
-  justify-content: flex-end;
-  align-items: center;
-  background: #1c819e;
-  height: 30px;
-  width: 100%;
+	position: absolute;
+	top: 0;
+	display: flex;
+	flex-direction: row;
+	justify-content: flex-end;
+	align-items: center;
+	background: #1c819e;
+	height: 30px;
+	width: 100%;
 
-  @media ${device.small_screen} {
-    height: 25px;
-  }
+	@media ${device.small_screen} {
+		height: 25px;
+	}
 
-  @media ${device.tablet} {
-    height: 25px;
-  }
+	@media ${device.tablet} {
+		height: 25px;
+	}
 
-  @media ${device.mobile} {
-    height: 20px;
-  }
+	@media ${device.mobile} {
+		height: 20px;
+	}
 
-  p {
-    font-family: "Roboto", sans-serif;
-    font-size: 0.8em;
-    color: #fff;
-    margin-right: 20px;
+	p {
+		font-family: "Roboto", sans-serif;
+		font-size: 0.8em;
+		color: #fff;
+		margin-right: 20px;
 
-    @media ${device.small_screen} {
-      font-size: 0.7em;
-      margin-right: 10px;
-    }
-    @media ${device.tablet} {
-      font-size: 0.7em;
-      margin-right: 10px;
-    }
+		@media ${device.small_screen} {
+			font-size: 0.7em;
+			margin-right: 10px;
+		}
+		@media ${device.tablet} {
+			font-size: 0.7em;
+			margin-right: 10px;
+		}
 
-    @media ${device.mobile} {
-      font-size: 0.6em;
-      margin-right: 20px;
-      margin-right: 5px;
-    }
-  }
+		@media ${device.mobile} {
+			font-size: 0.6em;
+			margin-right: 20px;
+			margin-right: 5px;
+		}
+	}
 `;
 
 export const HeaderBackground = styled.div`
-  position: absolute;
-  top: 30px;
-  display: flex;
-  justify-content: space-between;
-  background: #fff;
-  height: 145px;
-  width: 100%;
-  box-shadow: 0px 10px 10px rgba(0, 0, 0, 0.1);
+	position: absolute;
+	top: 30px;
+	display: flex;
+	justify-content: space-between;
+	background: #fff;
+	height: 145px;
+	width: 100%;
+	box-shadow: 0px 10px 10px rgba(0, 0, 0, 0.1);
 
-  @media ${device.small_screen} {
-    height: 100px;
-    top: 25px;
-  }
+	@media ${device.small_screen} {
+		height: 100px;
+		top: 25px;
+	}
 
-  @media ${device.tablet} {
-    height: 100px;
-    top: 25px;
-  }
+	@media ${device.tablet} {
+		height: 100px;
+		top: 25px;
+	}
 
-  @media ${device.mobile} {
-    height: 55px;
-    top: 20px;
-  }
+	@media ${device.mobile} {
+		height: 55px;
+		top: 20px;
+	}
 `;
 
 export const LogoContainer = styled.div`
-  display: flex;
-  justify-content: center;
-  height: 145px;
-  width: 15%;
+	display: flex;
+	justify-content: center;
+	height: 145px;
+	width: 15%;
 
-  @media ${device.small_screen} {
-    height: 100px;
-  }
+	@media ${device.small_screen} {
+		height: 100px;
+	}
 
-  @media ${device.tablet} {
-    margin-left: 20px;
-    height: 100px;
-    width: auto;
-  }
+	@media ${device.tablet} {
+		margin-left: 20px;
+		height: 100px;
+		width: auto;
+	}
 
-  @media ${device.mobile} {
-    margin-left: 20px;
-    height: 55px;
-    width: auto;
-  }
+	@media ${device.mobile} {
+		margin-left: 20px;
+		height: 55px;
+		width: auto;
+	}
 
-  img {
-    width: auto;
-    max-height: 120px;
-    margin: auto;
+	img {
+		width: auto;
+		max-height: 120px;
+		margin: auto;
 
-    @media ${device.desktop} {
-      max-width: 14vw;
-    }
+		@media ${device.desktop} {
+			max-width: 14vw;
+		}
 
-    @media ${device.small_screen} {
-      max-height: 140px;
-      width: 80%;
-    }
+		@media ${device.small_screen} {
+			max-height: 140px;
+			width: 80%;
+		}
 
-    @media ${device.tablet} {
-      height: 90px;
-      width: auto;
-    }
+		@media ${device.tablet} {
+			height: 90px;
+			width: auto;
+		}
 
-    @media ${device.mobile} {
-      height: 50px;
-      width: auto;
-    }
-  }
+		@media ${device.mobile} {
+			height: 50px;
+			width: auto;
+		}
+	}
 `;
 
 export const ContactContainer = styled.div`
-  display: flex;
-  justify-content: flex-end;
-  width: 70%;
+	display: flex;
+	justify-content: flex-end;
+	width: 70%;
 
-  @media ${device.tablet} {
-    display: none;
-  }
+	@media ${device.tablet} {
+		display: none;
+	}
 
-  @media ${device.mobile} {
-    display: none;
-  }
+	@media ${device.mobile} {
+		display: none;
+	}
 
-  img {
-    margin-top: 48px;
-    height: 20%;
-    margin-right: 10px;
-    margin-left: 40px;
+	img {
+		margin-top: 48px;
+		height: 20%;
+		margin-right: 10px;
+		margin-left: 40px;
 
-    @media ${device.small_screen} {
-      height: 20%;
-      margin-top: 25px;
-    }
-  }
+		@media ${device.small_screen} {
+			height: 20%;
+			margin-top: 25px;
+		}
+	}
 
-  p {
-    margin-top: 50px;
-    height: 20%;
-    margin-right: 20px;
-    font-family: "Roboto", sans-serif;
-    font-size: 1.5em;
-    color: #cfd7d6;
+	p {
+		margin-top: 50px;
+		height: 20%;
+		margin-right: 20px;
+		font-family: "Roboto", sans-serif;
+		font-size: 1.5em;
+		color: #cfd7d6;
 
-    @media ${device.small_screen} {
-      font-size: 1.2em;
-      margin-top: 25px;
-    }
-  }
+		@media ${device.small_screen} {
+			font-size: 1.2em;
+			margin-top: 25px;
+		}
+	}
 `;
