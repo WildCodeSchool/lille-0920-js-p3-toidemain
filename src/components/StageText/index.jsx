3f98437682a78e31c53ce53e69c95ba6def5d20f
gaetan-cordonnier <gaetan.cordonnier@gmail.com>
2021-01-12 11:26:24 +0100

    Component StageText created

---
+++ src/components/StageText/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function StageText() {
+	return (
+		<p>
+			Notre programme Stage up est une solution clé en main et innovante pour
+			les entreprises afin de répondre à la problématique d’accueil des jeunes
+			lors de leur stage de découverte en entreprise. On s’occupe de tout!
+		</p>
+	);
+}
---
+++ src/components/StageText/style.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const TextContainer = styled.div`
+	width: 320px;
+	height: 240px;
+
+	p {
+		font-family: "Roboto", sans-serif;
+		font-size: 1em;
+		color: #005542;
+	}
+`;
