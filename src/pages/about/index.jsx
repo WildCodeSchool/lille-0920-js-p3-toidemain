ccd3ed94638d394290bc0bf9bf0274aab4dd62a0
Thomas Druart <thomas.druart.59@gmail.com>
2021-01-18 11:13:10 +0100

    text changes

--- src/pages/about/index.jsx
+++ src/pages/about/index.jsx
@@ -15,14 +15,16 @@ const About = () => {
               <span>Notre mission : accompagner</span> les jeunes dans leur
               orientation professionnelle, <span>révèler</span> leurs talents
               pour leur permettre d’être
-              <span>libres, responsables et heureux</span>.
+              <span> libres, responsables et heureux</span>.
             </p>
             <p>
-              Ce qui nous anime : c’est permettre aux jeunes une première belle
-              rencontre avec le monde de l’entreprise qui leur donne envie.
-            </p>
-            <p>
-              Leur permettre de croire que <span>tout est possible</span>.
+              <span>Ce qui nous anime :</span>
+              <br />
+              Permettre aux jeunes une première belle rencontre avec le monde de
+              l’entreprise qui leur donne envie.
+              <br />
+              Leur permettre de croire que
+              <span> tout est possible</span>.
             </p>
           </div>
           <img src="/images/atelierbis.jpg" alt="atelier" />
