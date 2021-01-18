9486168ccee53aa96dcc4bd3ab1a2b69ab88b462
Guy de Messimy <gdemessimy@gmail.com>
2021-01-18 16:09:44 +0100

    add title

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -4,15 +4,19 @@ import Slider from "../../components/MySlider/Slider";
 import images from "../../components/MySlider/img/images";
 import { HomeContainer } from "./style";
 import Quote from "../../components/MyQuote/index";
+import KeyFacts from "../../components/KeyFacts/index";
+import Title from "../../components/Title/index"
 
 const Home = () => {
   return (
     <div>
+      <Title titleName={"my name"}/>
       <HomeContainer>
         <Slider slides={images} autoPlay={50} />
         <MenuButton />
       </HomeContainer>
       <Quote />
+      <KeyFacts />
     </div>
   );
 };
