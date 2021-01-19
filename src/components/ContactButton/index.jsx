ee3b3ec778f25686c92f8d3fb9b1a881052b8790
Guy de Messimy <gdemessimy@gmail.com>
2021-01-19 11:07:23 +0100

    create contact button for home page with css

---
+++ src/components/ContactButton/index.jsx
@@ -0,0 +1,11 @@
+import { MainButton } from "./style";
+
+const ContactButton = () => {
+  return (
+    <MainButton>
+      Contactez-nous <br />
+    </MainButton>
+  );
+};
+
+export default ContactButton;
---
+++ src/components/ContactButton/style.jsx
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+import { device } from "../../layout/Devices";
+
+export const MainButton = styled.button`
+  background-color: #ffbe00;
+  border: solid 1px #ffbe00;
+  border-radius: 10px 10px 10px 10px;
+  color: #fff;
+  font-size: 1.2em;
+  line-height: 1.8em;
+  letter-spacing: 1px;
+  padding: 15px 10px 15px 10px;
+  font-family: "Roboto", sans-serif;
+
+  @media ${device.mobile} {
+    font-size: 0.8em;
+  }
+  @media ${device.tablet} {
+    font-size: 1.2em;
+  }
+`;
