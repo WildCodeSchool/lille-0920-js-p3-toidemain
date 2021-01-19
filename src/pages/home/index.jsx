13fbed269280a12f0dcc0f61245c0a81fb4879d5
Guy de Messimy <gdemessimy@gmail.com>
2021-01-19 16:19:05 +0100

    delete config import at home

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -2,19 +2,14 @@ import React from "react";
 import MenuButton from "../../components/HomeButton";
 import Slider from "../../components/MySlider/Slider";
 import images from "../../components/MySlider/img/images";
-import { HomeContainer, ColonneA } from "./style";
+import { HomeContainer } from "./style";
 import Quote from "../../components/MyQuote/index";
-import EntrepriseText from "../../components/ButtonTextA";
-import Button from "../../components/InfoButton";
+
 
 const Home = () => {
   return (
     <div>
       <HomeContainer>
-        <ColonneA>
-         <EntrepriseText />
-        <Button />
-        </ColonneA>
         <Slider slides={images} autoPlay={50} />
         <MenuButton />
       </HomeContainer>
