9960e77861426681692eb213fa8a195e5edf78f7
gaetan-cordonnier <gaetan.cordonnier@gmail.com>
2021-01-19 16:27:42 +0100

    Fixed bug AppStyle

--- src/App.js
+++ src/App.js
@@ -7,26 +7,26 @@ import Header from "./components/Header/index";
 import Footer from "./components/Footer/index";
 import Pedagogie from "./components/Pedagogie/index";
 import Home from "./pages/home";
-import AppStyle from "./style";
+import { AppStyle } from "./style";
 
 function App() {
-  return (
-    <>
-      <Reset />
-      <AppStyle>
-        <Router>
-          <Header />
-          <Switch>
-            <Route exact path="/" component={Home} />
-            <Route exact path="/about" component={About} />
-            <Route exact path="/contacter" component={Contacter} />
-            <Route exact path="/action/pedagogie" component={Pedagogie} />
-          </Switch>
-          <Footer />
-        </Router>
-      </AppStyle>
-    </>
-  );
+	return (
+		<>
+			<Reset />
+			<AppStyle>
+				<Router>
+					<Header />
+					<Switch>
+						<Route exact path="/" component={Home} />
+						<Route exact path="/about" component={About} />
+						<Route exact path="/contacter" component={Contacter} />
+						<Route exact path="/action/pedagogie" component={Pedagogie} />
+					</Switch>
+					<Footer />
+				</Router>
+			</AppStyle>
+		</>
+	);
 }
 
 export default App;
