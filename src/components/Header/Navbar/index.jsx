6fef2ec8916bd84d840e0ead45c44342c9852e4c
AmjadLille <sammanamjad86@gmail.com>
2021-01-19 16:20:32 +0100

    fix nav bar link to page stageup

--- src/components/Header/Navbar/index.jsx
+++ src/components/Header/Navbar/index.jsx
@@ -1,6 +1,6 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import { NavContainer, NavDropdown, DropdownItems } from "./style";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { NavContainer, NavDropdown, DropdownItems } from './style';
 
 export default function Navbar() {
   return (
@@ -22,12 +22,10 @@ export default function Navbar() {
           <DropdownItems>
             <Link to="/action/pedagogie">Notre Pédagogie</Link>
             <Link to="/action/atelier">Nos Ateliers</Link>
-            <Link to="/stageup">Stage-Up</Link>
+            <Link to="/action/stageup">Stage-Up</Link>
           </DropdownItems>
         </NavDropdown>
-        |<a href="/soutien">Nous Soutenir</a>|
-        <a href="/actualite">Notre Actualité</a>|
-        <Link to="/contacter">Nous Contacter</Link>
+        |<a href="/soutien">Nous Soutenir</a>|<a href="/actualite">Notre Actualité</a>|<Link to="/contacter">Nous Contacter</Link>
       </nav>
     </NavContainer>
   );
