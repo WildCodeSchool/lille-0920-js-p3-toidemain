a2bfa056a8bf7a17cee8bd34206785a6b7f85f74
Guy de Messimy <gdemessimy@gmail.com>
2021-01-19 16:49:34 +0100

    fix app conflicts

--- src/App.js
+++ src/App.js
@@ -7,29 +7,12 @@ import Header from "./components/Header/index";
 import Footer from "./components/Footer/index";
 import Pedagogie from "./components/Pedagogie/index";
 import Home from "./pages/home";
-<<<<<<< HEAD
-import Atelier from "./components/Atelier/index";
-=======
 import AppStyle from "./style";
->>>>>>> eb01d0daf84c5117310618f5532bc7a9b7413fc0
 
 function App() {
   return (
     <>
       <Reset />
-<<<<<<< HEAD
-      <Router>
-        <Header />
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/about" component={About} />
-          <Route exact path="/contacter" component={Contacter} />
-          <Route exact path="/action/pedagogie" component={Pedagogie} />
-          <Route exact path="/action/atelier" component={Atelier} />
-        </Switch>
-        <Footer />
-      </Router>
-=======
       <AppStyle>
         <Router>
           <Header />
@@ -42,7 +25,6 @@ function App() {
           <Footer />
         </Router>
       </AppStyle>
->>>>>>> eb01d0daf84c5117310618f5532bc7a9b7413fc0
     </>
   );
 }
