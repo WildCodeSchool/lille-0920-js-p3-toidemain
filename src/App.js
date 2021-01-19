44690b1843933cc8619502a90f0bcfacb8be03dc
Amelie Macquet <amelie.macquet.am@gmail.com>
2021-01-19 16:54:11 +0100

    add route to soutenir file

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Header from "./components/Header/index";
 import Footer from "./components/Footer/index";
 import Pedagogie from "./components/Pedagogie/index";
 import Home from "./pages/home";
+import Soutenir from "./pages/soutenir"
 import AppStyle from "./style";
 
 function App() {
@@ -21,6 +22,7 @@ function App() {
             <Route exact path="/about" component={About} />
             <Route exact path="/contacter" component={Contacter} />
             <Route exact path="/action/pedagogie" component={Pedagogie} />
+            <Route exact path="/soutenir" component={Soutenir} />
           </Switch>
           <Footer />
         </Router>
--- src/pages/soutenir/index.jsx
+++ src/pages/soutenir/index.jsx
@@ -1,9 +1,14 @@
 import React from "react";
-
+import Title from "../../components/Title/index";
+import {SoutenirContainer} from "./style";
 
 const Soutenir = () => {
-    return (
-
-    )
-}    
+  return (
+    <div>
+      <SoutenirContainer>
+        <Title titleName="Nous soutenir" />
+      </SoutenirContainer>
+    </div>
+  );
+};
 export default Soutenir;
