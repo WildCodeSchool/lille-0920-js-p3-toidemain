ebc28d5c2a95a62d5428551eb48e23628ed4c6da
Thomas Druart <68994903+ThomasDruart@users.noreply.github.com>
2021-01-19 16:01:25 +0100

    Update index.jsx
    typo fixed on line 11

--- src/pages/about/index.jsx
+++ src/pages/about/index.jsx
@@ -8,7 +8,7 @@ const About = () => {
   return (
     <AboutContainer>
       <Pledge>
-        <Title titleName={"Notre Engagements"} />
+        <Title titleName={"Notre Engagement"} />
         <PledgeBox>
           <div>
             <p>
