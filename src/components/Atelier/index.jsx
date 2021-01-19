1fc3ec2d3cd32a81d430752e5e576f654358dbd3
Guy de Messimy <gdemessimy@gmail.com>
2021-01-19 09:26:06 +0100

    fix space and prettier

--- src/components/Atelier/index.jsx
+++ src/components/Atelier/index.jsx
@@ -24,18 +24,18 @@ export default function Atelier() {
           professionnelles
         </MainTitle>
         <Paragraph>
-          Vous êtes une association? Un établissement scolaire? Une fondation?
-          Une entreprise? Une collectivité? Toi demain propose et organise des{" "}
-          <SpanB>ateliers sur mesures </SpanB>pour accompagner les jeunes dans
-          leur orientation professionnelle et leur permettre de révéler leurs
-          talents! Forts de notre expérience avec le public des jeunes de{" "}
-          <SpanB>15 à 25 ans de tous horizons,</SpanB>
+          Vous êtes une association ? un établissement scolaire ? une fondation?
+          une entreprise ? une collectivité ? Toi demain propose et organise des
+          <SpanB>&nbsp;ateliers sur mesures </SpanB>pour accompagner les jeunes
+          dans leur orientation professionnelle et leur permettre de révéler
+          leurs talents ! fort de notre expérience avec le public des jeunes de
+          <SpanB>&nbsp;15 à 25 ans de tous horizons,&nbsp;</SpanB>
           nous nous adaptons à leurs besoins. Nos coachs de terrain aiment
           innover et co-créer de nouveaux programmes pour faire grandir les
-          jeunes en accord avec{" "}
+          jeunes en accord avec &nbsp;
           <Link to="/action/pedagogie">notre pédagogie.</Link>&nbsp; Ils
           interviennent régulièrement dans des grandes écoles , des universités,
-          dans des établissements scolaires et dans des associations.Nos mots
+          dans des établissements scolaires et dans des associations. Nos mots
           clés : <SpanB>apprendre en s'amusant! </SpanB>
         </Paragraph>
       </TextContainer>
