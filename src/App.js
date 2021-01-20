d0c16ddea501e6ab7757a641dbb08f50048f2427
AmjadLille <sammanamjad86@gmail.com>
2021-01-20 09:33:26 +0100

    pull dev

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import { Reset } from "styled-reset";
-import Contacter from "./pages/nous-contacter/index";
-import About from "./pages/about";
-import Header from "./components/Header/index";
-import Footer from "./components/Footer/index";
-import Pedagogie from "./components/Pedagogie/index";
-import Home from "./pages/home";
-import AppStyle from "./style";
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Reset } from 'styled-reset';
+import Contacter from './pages/nous-contacter/index';
+import About from './pages/about';
+import Header from './components/Header/index';
+import Footer from './components/Footer/index';
+import Pedagogie from './components/Pedagogie/index';
+import Home from './pages/home';
+import { AppStyle } from './style';
 
 function App() {
   return (
