94a89158aa090747eb55606b66987ca1785ddc14
Amelie Macquet <amelie.macquet.am@gmail.com>
2021-01-20 10:28:36 +0100

    add balise style the index

--- src/pages/soutenir/index.jsx
+++ src/pages/soutenir/index.jsx
@@ -1,30 +1,30 @@
 import React from "react";
 import Title from "../../components/Title/index";
-import { SoutenirContainer } from "./style";
+import { SoutenirContainer, Container1, Container2, Container3, Container4, Don, SoutenirBox, DonTitle, Contact, TitleBox, Text } from "./style";
 
 const Soutenir = () => {
   return (
-    <div>
+    <>
       <SoutenirContainer>
         <Title titleName="Nous soutenir" />
-        <div>
-          <div>
-            <h2>Vous êtes une fondation ? un fond de dotation ?</h2>
-            <p>
+        <SoutenirBox>
+          <Container1>
+            <TitleBox>Vous êtes une fondation ? un fond de dotation ?</TitleBox>
+            <Text>
               Vous pouvez nous faire un don pour financer un programme de 4
               matinées pour d'accompagnement les jeunes issus des quartiers
               prioritaires des Hauts de France. Exemple de contenu du programme:
               Travail sur la découverte des métiers et des talents. Nous
               travaillons en collaboration avec le tissu associatif de la
               région.
-            </p>
-          </div>
-          <div>
-            <h2>
+            </Text>
+          </Container1>
+          <Container2>
+            <TitleBox>
               Vous êtes une petite entreprise et vous voulez vous investir
               auprès des jeunes ?
-            </h2>
-            <p>
+            </TitleBox>
+            <Text>
               Vous pouvez nous faire don de votre taxe d'apprentissage pour
               co-financer un programme de 4 matinées pour d'accompagnement les
               jeunes issus des quartiers prioritaires. Exemple de contenu du
@@ -32,29 +32,29 @@ const Soutenir = () => {
               Nous travaillons en collaboration avec les associations de la
               région. Vous pouvez aussi faire intervenir vos collaborateurs dans
               ce projet : nous avons besoin de vous!
-            </p>
-          </div>
-          <div>
-            <h2>Vous êtes une entreprise de plus de 300 collaborateurs?</h2>
-            <p>
+            </Text>
+          </Container2>
+          <Container3>
+            <TitleBox>Vous êtes une entreprise de plus de 300 collaborateurs?</TitleBox>
+            <Text>
               Vous pouvez accueillir le programme "stage up" permettre aux
               enfants de vos collaborateurs d'accèder à un stage de qualité dans
               votre entreprise ou dans l'entreprise de l'un de nos partenaires.
               Ce programme est finançable à 100% via la taxe d'apprentissage.
-            </p>
-          </div>
-          <div>
-              <h2>Vous êtes un établissement scolaire ?</h2>
-              <p>Donnez la chance à vos élèves de participer au programme stage up!</p>
-              <button>Nous contacter</button>
-          </div>
-        </div>
-        <div>
-            <h3>Pour faire un don</h3>
-            <button>Nous contacter</button>
-        </div>
+            </Text>
+          </Container3>
+          <Container4>
+              <TitleBox>Vous êtes un établissement scolaire ?</TitleBox>
+              <Text>Donnez la chance à vos élèves de participer au programme stage up!</Text>
+              <Contact>Nous contacter</Contact>
+          </Container4>
+        </SoutenirBox>
+        <Don>
+            <DonTitle>Pour faire un don</DonTitle>
+            <Contact>Nous contacter</Contact>
+        </Don>
       </SoutenirContainer>
-    </div>
+    </>
   );
 };
 export default Soutenir;
--- src/pages/soutenir/style.js
+++ src/pages/soutenir/style.js
@@ -2,6 +2,35 @@ import styled from "styled-components";
 
 
 export const SoutenirContainer = styled.div`
-height: 100vh;
-background-color: green;
+
+`
+export const Container1 = styled.div`
+
+`
+export const Container2 = styled.div`
+
+`
+export const Container3 = styled.div`
+
+`
+export const Container4 = styled.div`
+
+`
+export const Don = styled.div`
+
+`
+export const SoutenirBox = styled.div`
+
+`
+export const DonTitle = styled.h3`
+
+`
+export const Contact = styled.button`
+
+`
+export const TitleBox = styled.h2`
+
+`
+export const Text = styled.p`
+
 `
