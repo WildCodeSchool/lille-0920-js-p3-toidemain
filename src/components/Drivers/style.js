8a4a378c788a66e7ba532989903bdfa8208012ee
gaetan-cordonnier <gaetan.cordonnier@gmail.com>
2021-01-20 14:42:29 +0100

    Prettier command

--- src/components/Drivers/style.js
+++ src/components/Drivers/style.js
@@ -2,48 +2,48 @@ import styled from "styled-components";
 import { device } from "../../layout/Devices";
 
 export const Container = styled.div`
-	@media ${device.mobile} {
-		display: none;
-	}
+  @media ${device.mobile} {
+    display: none;
+  }
 `;
 
 export const DriversStyle = styled.div`
-	background-image: url(/images/animated_background.svg);
-	font-family: "Roboto", sans-serif;
-	display: flex;
-	flex-direction: row;
-	justify-content: center;
+  background-image: url(/images/animated_background.svg);
+  font-family: "Roboto", sans-serif;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
 
-	svg g {
-		fill: #1c819e;
-	}
+  svg g {
+    fill: #1c819e;
+  }
 
-	svg g:hover {
-		fill: #91ccdb;
-		cursor: pointer;
-	}
+  svg g:hover {
+    fill: #91ccdb;
+    cursor: pointer;
+  }
 
-	svg g polygon {
-		stroke: #d7d7d7;
-		stroke-width: 0.3;
-		transition: fill-opacity 0.5s;
-	}
+  svg g polygon {
+    stroke: #d7d7d7;
+    stroke-width: 0.3;
+    transition: fill-opacity 0.5s;
+  }
 
-	svg g text {
-		font-size: 0.11em;
-		text-transform: uppercase;
-		fill: #fff;
-	}
+  svg g text {
+    font-size: 0.11em;
+    text-transform: uppercase;
+    fill: #fff;
+  }
 
-	.hexBlue g {
-		fill: #1c819e;
-	}
+  .hexBlue g {
+    fill: #1c819e;
+  }
 
-	.hexGreen g {
-		fill: #005542;
-	}
+  .hexGreen g {
+    fill: #005542;
+  }
 
-	.hexYellow g {
-		fill: #ffbe00;
-	}
+  .hexYellow g {
+    fill: #ffbe00;
+  }
 `;
