0692febb9645bcb1ccb04d3945717ac2536d0dcd
Amelie Macquet <amelie.macquet.am@gmail.com>
2021-01-21 10:58:36 +0100

    change style to the page soutenir

--- src/pages/soutenir/index.jsx
+++ src/pages/soutenir/index.jsx
@@ -1,6 +1,25 @@
 import React from "react";
 import Title from "../../components/Title/index";
-import { SoutenirContainer, Container1, Container2, Container3, Container4, Don, SoutenirBox, DonTitle, Contact, TitleBox, Text } from "./style";
+import {
+  SoutenirContainer,
+  Container1,
+  Container2,
+  Container3,
+  Container4,
+  Don,
+  SoutenirBox,
+  DonTitle,
+  Contact1,
+  Contact,
+  TitleBox1,
+  TitleBox2,
+  TitleBox3,
+  TitleBox4,
+  Text1, Text2,
+  Text3,
+  Text4,
+  Img
+} from "./style";
 
 const Soutenir = () => {
   return (
@@ -9,22 +28,22 @@ const Soutenir = () => {
         <Title titleName="Nous soutenir" />
         <SoutenirBox>
           <Container1>
-            <TitleBox>Vous êtes une fondation ? un fond de dotation ?</TitleBox>
-            <Text>
+            <TitleBox1>Vous êtes une fondation ? un fond de dotation ?</TitleBox1>
+            <Text1>
               Vous pouvez nous faire un don pour financer un programme de 4
               matinées pour d'accompagnement les jeunes issus des quartiers
               prioritaires des Hauts de France. Exemple de contenu du programme:
               Travail sur la découverte des métiers et des talents. Nous
               travaillons en collaboration avec le tissu associatif de la
               région.
-            </Text>
+            </Text1>
           </Container1>
           <Container2>
-            <TitleBox>
+            <TitleBox2>
               Vous êtes une petite entreprise et vous voulez vous investir
               auprès des jeunes ?
-            </TitleBox>
-            <Text>
+            </TitleBox2>
+            <Text2>
               Vous pouvez nous faire don de votre taxe d'apprentissage pour
               co-financer un programme de 4 matinées pour d'accompagnement les
               jeunes issus des quartiers prioritaires. Exemple de contenu du
@@ -32,26 +51,31 @@ const Soutenir = () => {
               Nous travaillons en collaboration avec les associations de la
               région. Vous pouvez aussi faire intervenir vos collaborateurs dans
               ce projet : nous avons besoin de vous!
-            </Text>
+            </Text2>
           </Container2>
           <Container3>
-            <TitleBox>Vous êtes une entreprise de plus de 300 collaborateurs?</TitleBox>
-            <Text>
+            <TitleBox3>
+              Vous êtes une entreprise de plus de 300 collaborateurs?
+            </TitleBox3>
+            <Text3>
               Vous pouvez accueillir le programme "stage up" permettre aux
               enfants de vos collaborateurs d'accèder à un stage de qualité dans
               votre entreprise ou dans l'entreprise de l'un de nos partenaires.
               Ce programme est finançable à 100% via la taxe d'apprentissage.
-            </Text>
+            </Text3>
+            <Img src="/images/stageUp.png" alt="logo lien Stage Up" />
           </Container3>
           <Container4>
-              <TitleBox>Vous êtes un établissement scolaire ?</TitleBox>
-              <Text>Donnez la chance à vos élèves de participer au programme stage up!</Text>
-              <Contact>Nous contacter</Contact>
+            <TitleBox4>Vous êtes un établissement scolaire ?</TitleBox4>
+            <Text4>
+              Donnez la chance à vos élèves de participer au programme stage up!
+            </Text4>
+            <Contact1>Nous contacter</Contact1>
           </Container4>
         </SoutenirBox>
         <Don>
-            <DonTitle>Pour faire un don</DonTitle>
-            <Contact>Nous contacter</Contact>
+          <DonTitle>Pour faire un don</DonTitle>
+          <Contact>Nous contacter</Contact>
         </Don>
       </SoutenirContainer>
     </>
--- src/pages/soutenir/style.js
+++ src/pages/soutenir/style.js
@@ -1,36 +1,138 @@
 import styled from "styled-components";
 
-
-export const SoutenirContainer = styled.div`
-
-`
+export const SoutenirContainer = styled.div``;
 export const Container1 = styled.div`
-
-`
+width: 25%;
+height: 45vh;
+background-color: white;
+border-radius: 5px;
+margin: 1vw;
+padding: 1vh;
+}
+`;
 export const Container2 = styled.div`
-
-`
+  width: 25%;
+  background-color: white;
+  border-radius: 5px;
+  margin: 1vw;
+  padding: 1vh;
+`;
 export const Container3 = styled.div`
-
-`
+display: flex;
+flex-direction: column;
+align-items:center;
+  width: 25%;
+  height: 45vh;
+  background-color: white;
+  border-radius: 5px;
+  margin: 1vw;
+  padding: 1vh;
+`;
 export const Container4 = styled.div`
-
-`
+  width: 25%;
+  border-radius: 5px;
+  background-color: rgb(255, 190, 0);
+  margin: 1vw;
+  padding: 1vh;
+`;
 export const Don = styled.div`
-
-`
+  display: flex;
+  flex-direction: column;
+  align-items:center;
+  padding: 5vw;
+  width: 25%;
+  margin-left:  33vw;
+`;
 export const SoutenirBox = styled.div`
-
-`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: nowrap;
+  margin-top: 5vh;
+`;
 export const DonTitle = styled.h3`
-
-`
+  font-size: 1.8rem;
+  color: #145264;
+  text-align: center;
+`;
+export const Contact1 = styled.button`
+  display: flex;
+  justify-content: center;
+  margin: 4.6vw;
+  font-size: 22px;
+  color: white;
+  text-decoration: none;
+  background-color: transparent;
+  transition: all 0.5s ease;
+  border: 5px solid white;
+  border-radius: 20px;
+  padding: 0.5rem;
+  &:hover {
+    background-color: #ffbe00;
+    color: #005542;
+  }
+`;
+export const TitleBox4 = styled.h2`
+  font-size: 1.6rem;
+  text-align: center;
+  color: white;
+`;
+export const Text4 = styled.p`
+  font-size: 1.3rem;
+  margin: 2vw;
+  margin-top: 7vh;
+  text-align: justify;
+`;
+export const Img = styled.img`
+  width: 30%;
+  margin-top: -3vh;
+`;
+export const Text1 = styled.p`
+  font-size: 1.2rem;
+  margin: 1vw;
+  margin-top: 6vh;
+  text-align: justify;
+`;
+export const Text2 = styled.p`
+  margin: 1vw;
+  margin-top: 3.5vh;
+  font-size: 1.1rem;
+  text-align: justify;
+`;
+export const Text3 = styled.p`
+  margin: 2vw;
+  font-size: 1.1rem;
+  margin-top: 6vh;
+  text-align: justify;
+`;
+export const TitleBox3 = styled.h2`
+  color: #145264;
+  text-align: center;
+  font-size: 1.6rem;
+`;
+export const TitleBox2 = styled.h2`
+  color: #1c819e;
+  text-align: center;
+  font-size: 1.5rem;
+`;
+export const TitleBox1 = styled.h2`
+  color: #005542;
+  text-align: center;
+  font-size: 1.6rem;
+`;
 export const Contact = styled.button`
-
-`
-export const TitleBox = styled.h2`
-
-`
-export const Text = styled.p`
-
-`
+display: flex;
+  justify-content: center;
+  margin: 2vw;
+  width:50%;
+  font-size: 22px;
+  color: #145264;
+  text-decoration: none;
+  background-color: transparent;
+  transition: all 0.5s ease;
+  border: 5px solid #145264;
+  border-radius: 20px;
+  padding: 0.5rem;
+  &:hover {
+    background-color: #ffbe00;
+    color: #005542;
+  }`;
