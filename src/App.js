2818680db1a2c2a8d4dd32e67986ad705768508a
Amelie Macquet <amelie.macquet.am@gmail.com>
2021-01-22 19:45:45 +0100

    prettify

--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ import Header from "./components/Header/index";
 import Footer from "./components/Footer/index";
 import Pedagogie from "./components/Pedagogie/index";
 import Home from "./pages/home";
-import Soutenir from "./pages/soutenir"
-import {AppStyle} from "./style";
+import Soutenir from "./pages/soutenir";
+import { AppStyle } from "./style";
 
 function App() {
   return (
