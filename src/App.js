b3a7c9721e2f2bd953857beb08e8470d97fd8951
AmjadLille <sammanamjad86@gmail.com>
2021-01-27 14:11:31 +0100

    pull admin

--- src/App.js
+++ src/App.js
@@ -1,29 +1,16 @@
-<<<<<<< HEAD
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { Reset } from 'styled-reset';
 import Contacter from './pages/nous-contacter/index';
-import About from './pages/about';
-import Header from './components/Header/index';
+import About from './pages/about/index';
 import Footer from './components/Footer/index';
+import Header from './components/Header/index';
+import StageUp from './pages/stageUp/index';
 import Pedagogie from './components/Pedagogie/index';
+import Atelier from './components/Atelier';
 import Home from './pages/home';
+import Soutenir from './pages/soutenir';
 import { AppStyle } from './style';
-=======
-import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import { Reset } from "styled-reset";
-import Contacter from "./pages/nous-contacter/index";
-import About from "./pages/about/index";
-import Footer from "./components/Footer/index";
-import Header from "./components/Header/index";
-import StageUp from "./pages/stageUp/index";
-import Pedagogie from "./components/Pedagogie/index";
-import Atelier from "./components/Atelier";
-import Home from "./pages/home";
-import Soutenir from "./pages/soutenir";
-import { AppStyle } from "./style";
->>>>>>> 9db98386ef0e21f40ef8f5c454f36b01d1031d87
 
 function App() {
   return (
