1ec7e36e25e10d5647bd35672838f9ae2315418c
gaetan-cordonnier <gaetan.cordonnier@gmail.com>
2021-01-29 10:29:19 +0100

    Prettify

--- src/App.js
+++ src/App.js
@@ -14,27 +14,27 @@ import Soutenir from "./pages/soutenir";
 import { AppStyle } from "./style";
 
 function App() {
-	return (
-		<>
-			<Reset />
-			<AppStyle>
-				<Router>
-					<Header />
-					<Switch>
-						<Route exact path="/" component={Home} />
-						<Route exact path="/about" component={About} />
-						<Route exact path="/contacter" component={Contacter} />
-						<Route exact path="/action/stageup" component={StageUp} />
-						<Route exact path="/action/pedagogie" component={Pedagogie} />
-						<Route exact path="/soutenir" component={Soutenir} />
-						<Route exact path="/action/atelier" component={Atelier} />
-						<Route exact path="/login" component={Login} />
-					</Switch>
-					<Footer />
-				</Router>
-			</AppStyle>
-		</>
-	);
+  return (
+    <>
+      <Reset />
+      <AppStyle>
+        <Router>
+          <Header />
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/about" component={About} />
+            <Route exact path="/contacter" component={Contacter} />
+            <Route exact path="/action/stageup" component={StageUp} />
+            <Route exact path="/action/pedagogie" component={Pedagogie} />
+            <Route exact path="/soutenir" component={Soutenir} />
+            <Route exact path="/action/atelier" component={Atelier} />
+            <Route exact path="/login" component={Login} />
+          </Switch>
+          <Footer />
+        </Router>
+      </AppStyle>
+    </>
+  );
 }
 
 export default App;
--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -2,28 +2,28 @@ import React from "react";
 import { Container, Formulaire } from "./style";
 
 export default function Login() {
-	return (
-		<Container>
-			<Formulaire>
-				<h2>ADMINISTRATION</h2>
-				<input
-					name="emailaddress"
-					placeholder="Email"
-					className="email"
-					type="email"
-					required
-				/>
-				<input
-					name="password"
-					placeholder="Mot de passe"
-					type="password"
-					minLength="8"
-					required
-				/>
-				<button name="submit" className="btn" type="submit" value="Connexion">
-					Connexion
-				</button>
-			</Formulaire>
-		</Container>
-	);
+  return (
+    <Container>
+      <Formulaire>
+        <h2>ADMINISTRATION</h2>
+        <input
+          name="emailaddress"
+          placeholder="Email"
+          className="email"
+          type="email"
+          required
+        />
+        <input
+          name="password"
+          placeholder="Mot de passe"
+          type="password"
+          minLength="8"
+          required
+        />
+        <button name="submit" className="btn" type="submit" value="Connexion">
+          Connexion
+        </button>
+      </Formulaire>
+    </Container>
+  );
 }
