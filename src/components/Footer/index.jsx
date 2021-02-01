2e5d4188c8b907056cf35baa37aa5eb675ec9d00
gaetan-cordonnier <gaetan.cordonnier@gmail.com>
2021-02-01 11:32:41 +0100

    Add links facebook and instagram on footer component

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,57 +1,69 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import {
-  FooterContainer,
-  Info,
-  Adress,
-  Right,
-  Menu,
-  Social,
-  Icons,
+	FooterContainer,
+	Info,
+	Adress,
+	Right,
+	Menu,
+	Social,
+	Icons,
 } from "./style";
 
 const Footer = () => {
-  return (
-    <FooterContainer>
-      <Info>
-        <h3>Toi Demain</h3>
-        <Adress>
-          <p>
-            <span>
-              <img src="/images/icons/location-pin.png" alt="gps" />
-            </span>
-            &nbsp;&nbsp;59390 Sailly-lez-Lannoy
-          </p>
-        </Adress>
-        <p>
-          <span>
-            <img src="/images/icons/telephone.png" alt="tel" />
-          </span>
-          &nbsp;&nbsp;07 50 99 82 88
-        </p>
-        <p>
-          <span>
-            <img src="/images/icons/email.png" alt="email" />
-          </span>
-          &nbsp;&nbsp;contact@toidemain.fr
-        </p>
-      </Info>
-      <Menu>
-        <Social>
-          <h3>Retrouvez-nous sur</h3>
-        </Social>
-        <Icons>
-          <img src="/images/icons/instagram.png" alt="instagram" />
-          <img src="/images/icons/facebook.png" alt="facebook" />
-          <img src="/images/icons/linkedin.png" alt="linkedin" />
-        </Icons>
-        <p>&#169; 2021 Tous droits réservés Association Toi Demain</p>
-      </Menu>
-      <Right>
-        <Link to="/contacter">Nous Contacter</Link>
-      </Right>
-    </FooterContainer>
-  );
+	return (
+		<FooterContainer>
+			<Info>
+				<h3>Toi Demain</h3>
+				<Adress>
+					<p>
+						<span>
+							<img src="/images/icons/location-pin.png" alt="gps" />
+						</span>
+						&nbsp;&nbsp;59390 Sailly-lez-Lannoy
+					</p>
+				</Adress>
+				<p>
+					<span>
+						<img src="/images/icons/telephone.png" alt="tel" />
+					</span>
+					&nbsp;&nbsp;07 50 99 82 88
+				</p>
+				<p>
+					<span>
+						<img src="/images/icons/email.png" alt="email" />
+					</span>
+					&nbsp;&nbsp;contact@toidemain.fr
+				</p>
+			</Info>
+			<Menu>
+				<Social>
+					<h3>Retrouvez-nous sur</h3>
+				</Social>
+				<Icons>
+					<a
+						href="https://www.instagram.com/toi.demain/"
+						title="instagram"
+						target="blank"
+					>
+						<img src="/images/icons/instagram.png" alt="instagram" />
+					</a>
+					<a
+						href="https://www.facebook.com/toi.demain.5/"
+						title="facebook"
+						target="blank"
+					>
+						<img src="/images/icons/facebook.png" alt="facebook" />
+					</a>
+					<img src="/images/icons/linkedin.png" alt="linkedin" />
+				</Icons>
+				<p>&#169; 2021 Tous droits réservés Association Toi Demain</p>
+			</Menu>
+			<Right>
+				<Link to="/contacter">Nous Contacter</Link>
+			</Right>
+		</FooterContainer>
+	);
 };
 
 export default Footer;
