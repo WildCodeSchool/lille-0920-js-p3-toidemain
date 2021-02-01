7524d5a52937088acd571835ea8e1f602aba4656
AmjadLille <sammanamjad86@gmail.com>
2021-02-01 12:29:01 +0100

    fix bug

--- src/pages/soutenir/index.jsx
+++ src/pages/soutenir/index.jsx
@@ -1,28 +1,7 @@
-import React from "react";
-import Title from "../../components/Title/index";
-import { Link } from "react-router-dom";
-import {
-  SoutenirContainer,
-  Container1,
-  Container2,
-  Container3,
-  Container4,
-  SoutenirBox,
-  TitleBox1,
-  TitleBox2,
-  TitleBox3,
-  TitleBox4,
-  Text1,
-  Text2,
-  Text3,
-  Text4,
-  Img,
-  BContainer,
-  Liste,
-  BigLetter,
-  Button,
-  Button1,
-} from "./style";
+import React from 'react';
+import Title from '../../components/Title/index';
+import { Link } from 'react-router-dom';
+import { SoutenirContainer, Container1, Container2, Container3, Container4, SoutenirBox, TitleBox1, TitleBox2, TitleBox3, TitleBox4, Text1, Text2, Text3, Text4, Img, BContainer, Liste, BigLetter, Button, Button1 } from './style';
 
 const Soutenir = () => {
   return (
@@ -33,24 +12,15 @@ const Soutenir = () => {
           <TitleBox1>Vous êtes une fondation ? un fond de dotation ?</TitleBox1>
           <Text1>
             <ul>
-              <Liste>
-                Vous pouvez nous faire un don pour financer un programme de 4
-                matinées afin d'accompagner les jeunes issus des quartiers
-                prioritaires des Hauts de France.
-              </Liste>
+              <Liste>Vous pouvez nous faire un don pour financer un programme de 4 matinées afin d'accompagner les jeunes issus des quartiers prioritaires des Hauts de France.</Liste>
               <Liste>
                 <BigLetter>Exemple de contenu du programme :</BigLetter>
               </Liste>
               <Liste>
-                <BigLetter>
-                  • Travail sur la découverte des métiers et des talents.
-                </BigLetter>
+                <BigLetter>• Travail sur la découverte des métiers et des talents.</BigLetter>
               </Liste>
               <Liste>
-                <BigLetter>
-                  • Nous travaillons en collaboration avec le tissu associatif
-                  de la région.
-                </BigLetter>
+                <BigLetter>• Nous travaillons en collaboration avec le tissu associatif de la région.</BigLetter>
               </Liste>
             </ul>
           </Text1>
@@ -59,37 +29,21 @@ const Soutenir = () => {
           </Button>
         </Container1>
         <Container2>
-          <TitleBox2>
-            Vous êtes une petite entreprise et vous voulez vous investir auprès
-            des jeunes ?
-          </TitleBox2>
+          <TitleBox2>Vous êtes une petite entreprise et vous voulez vous investir auprès des jeunes ?</TitleBox2>
           <Text2>
             <ul>
-              <Liste>
-                Vous pouvez nous faire don de votre taxe d'apprentissage pour
-                co-financer un programme de 4 matinées afin d'accompagner les
-                jeunes issus des quartiers prioritaires.
-              </Liste>
+              <Liste>Vous pouvez nous faire don de votre taxe d'apprentissage pour co-financer un programme de 4 matinées afin d'accompagner les jeunes issus des quartiers prioritaires.</Liste>
               <Liste>
                 <BigLetter>Exemple de contenu du programme :</BigLetter>
               </Liste>
               <Liste>
-                <BigLetter>
-                  • Travail sur la découverte des métiers et des talents.
-                </BigLetter>
+                <BigLetter>• Travail sur la découverte des métiers et des talents.</BigLetter>
               </Liste>
               <Liste>
-                <BigLetter>
-                  • Nous travaillons en collaboration avec les associations de
-                  la région.
-                </BigLetter>
+                <BigLetter>• Nous travaillons en collaboration avec les associations de la région.</BigLetter>
               </Liste>
               <Liste>
-                <BigLetter>
-                  {" "}
-                  • Vous pouvez aussi faire intervenir vos collaborateurs dans
-                  ce projet :
-                </BigLetter>
+                <BigLetter>• Vous pouvez aussi faire intervenir vos collaborateurs dans ce projet :</BigLetter>
               </Liste>
               <Liste>Nous avons besoin de vous !</Liste>
             </ul>
@@ -101,21 +55,11 @@ const Soutenir = () => {
 
         <BContainer>
           <Container3>
-            <TitleBox3>
-              Vous êtes une entreprise de plus de 300 collaborateurs ?
-            </TitleBox3>
+            <TitleBox3>Vous êtes une entreprise de plus de 300 collaborateurs ?</TitleBox3>
             <Text3>
               <ul>
-                <Liste>
-                  Vous pouvez accueillir le programme "stage up" pour permettre
-                  aux enfants de vos collaborateurs d'accèder à un stage de
-                  qualité dans votre entreprise ou dans celle de l'un de nos
-                  partenaires.
-                </Liste>
-                <Liste>
-                  Ce programme est finançable à 100% via la taxe
-                  d'apprentissage.
-                </Liste>
+                <Liste>Vous pouvez accueillir le programme "stage up" pour permettre aux enfants de vos collaborateurs d'accèder à un stage de qualité dans votre entreprise ou dans celle de l'un de nos partenaires.</Liste>
+                <Liste>Ce programme est finançable à 100% via la taxe d'apprentissage.</Liste>
               </ul>
             </Text3>
             <Link to="/action/stageup">
@@ -124,10 +68,7 @@ const Soutenir = () => {
           </Container3>
           <Container4>
             <TitleBox4>Vous êtes un étabListessement scolaire ?</TitleBox4>
-            <Text4>
-              Donnez la chance à vos élèves de participer au programme stage up
-              !
-            </Text4>
+            <Text4>Donnez la chance à vos élèves de participer au programme stage up !</Text4>
             <Link to="/action/stageup">
               <Img src="/images/stageUpWhite.png" alt="logo Listeen Stage Up" />
             </Link>
