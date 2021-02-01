d0bf54ae2a6e609ca236a09bf645fbcb11ba6d4e
gaetan-cordonnier <gaetan.cordonnier@gmail.com>
2021-02-01 11:14:16 +0100

    Delete contact link on header component

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,38 +1,33 @@
 import React from "react";
 import {
-  HeaderContainer,
-  TopBorder,
-  HeaderBackground,
-  LogoContainer,
-  ContactContainer,
+	HeaderContainer,
+	TopBorder,
+	HeaderBackground,
+	LogoContainer,
 } from "./style";
 import Navbar from "./Navbar/index";
 import Burger from "./Burger/index";
 import { Link } from "react-router-dom";
 
 const Header = () => {
-  return (
-    <>
-      <Burger />
-      <HeaderContainer>
-        <TopBorder>
-          <p>Association déclarée - SIREN: 852 306 703 00012</p>
-        </TopBorder>
-        <HeaderBackground>
-          <LogoContainer>
-            <Link to="/">
-              <img src="/images/logo_Header.png" alt="ToiDemain logo" />
-            </Link>
-          </LogoContainer>
-          <ContactContainer>
-            <img src="/images/icons/mail_icon.svg" alt="e-mail icon" />
-            <p>contact@toidemain.fr</p>
-          </ContactContainer>
-        </HeaderBackground>
-        <Navbar />
-      </HeaderContainer>
-    </>
-  );
+	return (
+		<>
+			<Burger />
+			<HeaderContainer>
+				<TopBorder>
+					<p>Association déclarée - SIREN: 852 306 703 00012</p>
+				</TopBorder>
+				<HeaderBackground>
+					<LogoContainer>
+						<Link to="/">
+							<img src="/images/logo_Header.png" alt="ToiDemain logo" />
+						</Link>
+					</LogoContainer>
+				</HeaderBackground>
+				<Navbar />
+			</HeaderContainer>
+		</>
+	);
 };
 
 export default Header;
